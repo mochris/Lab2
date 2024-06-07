5390928b5bb39f3d6017a83469fe0ca0a3e79277
mochris <mochris@snu.ac.kr>
2024-06-07 07:29:24 +0000

    main.cpp finished

--- main.cpp
+++ main.cpp
@@ -7,13 +7,24 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+   if(cnt>=M){
+      for(int i=0;i<vec.size();i++){
+         cout<< vec[i]<<" ";
+      }
+      cout<<endl;
+      return;
+   }
+   int start;
+   if(vec.size()==0){
+      start = 1;
+   }else{
+      start = vec.back()+1;
+   }
+   for(int i=start;i<=N;i++){
+      vec.push_back(i);
+      func(cnt+1);
+      vec.erase(vec.end()-1);
+   }
 }
 
 int main() {
